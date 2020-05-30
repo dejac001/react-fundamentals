2e3de1b0f7367155e8adce59ab9f09418a51b13e
Rob DeJaco <dejac001@umn.edu>
2020-05-30 11:42:58 -0400

    React JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const title = 'React'; 
+// const welcome = {
+  // greeting: 'Hey',
+  // title: 'React'
+// }
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   // commonly called a function component; its just a Javascript function
@@ -8,9 +14,14 @@ function App() {
   // returns code that resembles HTML which is called *JSX*
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>
+        Hello {getTitle('React')}
+        {/* {welcome.greeting} {welcome.title} */}
+      </h1>
+      <label htmlFor="search">Search: </label> 
+      <input id="search" type="text"/>
     </div>
-  )
+  ) // *htmlFor* reflects the *for* attribute in HTML. JSX replaces a handful of internal HTML attributes, but you cand
 }
 
 export default App
