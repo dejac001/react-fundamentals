ae156f9222ab76444b3f1c745637cb0d900d9bac
Rob DeJaco <dejac001@umn.edu>
2020-05-30 12:30:17 -0400

    React Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
 
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ]
+
   const handleChange = event => {
     console.log(event.target.value);
   };
@@ -33,15 +34,16 @@ const App = () => {
 
       <hr />
 
-      <List />
+      {/* Use React props to pass the array to the List component */}
+      <List list={stories} /> 
     </div>
   ); // *htmlFor* reflects the *for* attribute in HTML. JSX replaces a handful of internal HTML attributes, but you cand
 };
 
 
 // List component
-const List = () => 
-  list.map(item => (
+const List = props =>  {/* change to props to prevent the list/stories variable from polluting the global scope */}
+  props.list.map(item => (
       <div key={item.objectID}>
         <span>
           <a href={item.url}>{item.title}</a>
