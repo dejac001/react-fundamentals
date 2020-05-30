8a84b493c5ab3a8844f49b6e537b580ffe0cfe13
Rob DeJaco <dejac001@umn.edu>
2020-05-30 11:53:34 -0400

    Lists in React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
-// const welcome = {
-  // greeting: 'Hey',
-  // title: 'React'
-// }
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
 
 function App() {
   // commonly called a function component; its just a Javascript function
@@ -14,12 +25,24 @@ function App() {
   // returns code that resembles HTML which is called *JSX*
   return (
     <div>
-      <h1>
-        Hello {getTitle('React')}
-        {/* {welcome.greeting} {welcome.title} */}
-      </h1>
+      <h1>My Hacker Stories</h1>
+
       <label htmlFor="search">Search: </label> 
       <input id="search" type="text"/>
+
+      <hr />
+      {list.map(function(item) {
+        return (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+        );
+      })}
     </div>
   ) // *htmlFor* reflects the *for* attribute in HTML. JSX replaces a handful of internal HTML attributes, but you cand
 }
