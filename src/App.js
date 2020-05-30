271dadebc7b8b9d999cec82e9430924b6f20da80
Rob DeJaco <dejac001@umn.edu>
2020-05-30 11:35:54 -0400

    Meet the react component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
+const title = 'React'; 
+
 function App() {
+  // commonly called a function component; its just a Javascript function
+  // doesnt have any parameters in its function signature yet
+  // returns code that resembles HTML which is called *JSX*
   return (
     <div>
       <h1>Hello World</h1>
